6d9c41e6d3e80cf05d92acc9f59ad65ec776de50
Henrique <fernandes4626@gmail.com>
2025-05-26 22:37:49 -0300

    feat: add default layout style

--- src/app/components/default-layout/default-layout.component.ts
+++ src/app/components/default-layout/default-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-default-layout',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './default-layout.component.scss'
 })
 export class DefaultLayoutComponent {
-
+  @Input() title: string = '';
+  @Input() primaryButtonText: string = '';
+  @Input() secondaryButtonText: string = '';
 }
